114e0320ef6fa0add481b8dd1407c2b027dbaf25
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-11-02 18:42:08 +0530

    header nav ul added
    - custom clay classes added to tailwind configuration
    clay card and clay button
    - roboto mono font added
    - header is now fixed
    - blobs SVG added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { Header } from './components/common/Header'
+import { HeroSection } from './components/landing/HeroSection'
 
 function App() {
 
 
   return (
     <>
-      <Header/> 
+      <div className='relative'>
+        <Header/> 
+      </div>
+      <HeroSection/>
     </>
   )
 }
