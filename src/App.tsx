a47c689c5a68bb99f0c7c2d1d803421e4f1a4a1a
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-11-02 13:06:47 +0530

    tailwind bug fixed
    tested custom color palettes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Header } from './components/common/Header'
 
 function App() {
 
 
   return (
     <>
-    
+      <Header/> 
     </>
   )
 }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,9 @@
 
 export const Header = () => {
   return (
-    <div className="bg-delft_blue-700">Header</div>
+    <>
+      <div className="text-delft_blue-700">Something </div>
+      <div className="h-[5rem] w-[5rem] bg-red_crayola-400">.</div>
+    </>
   )
 }
