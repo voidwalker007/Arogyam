8ef28fac938e6f4af8104859051221b9b6d8f320
Aman Singh Bhadauria <36358196+notavacillator@users.noreply.github.com>
2023-11-03 18:53:21 +0530

    Addition/landing (#6)
    * landing page draft
    
    - images added
    - main button card in landing page added
    
    * - design fixed for hero
    
    images added

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className='relative flex '>
+      <div className=' relative flex '>
         <div className='fixed top-1 left-1'>
           {/* <img src="./src/assets/3d-logo-2.png" alt="3d-logo" height={200} width={140} className='object-fill'/> */}
         </div>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,8 +3,8 @@ export const Header = () => {
   return (
     <>
       <header className="relative z-[999] bg-white">
-        <nav className=" fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2  
-                    sm:top-[1.7rem] sm:h-[initial]  lg:w-[50%] w-[80%] mx-auto  rounded-full 
+        <nav className=" fixed top-[1.5rem] left-1/2 h-12 -translate-x-1/2  
+                    sm:top-[1.7rem] sm:h-[initial]  lg:w-[60%] w-[60%] mx-auto  rounded-full 
                   clay-card  py-[0.2rem]">
           <ul className="flex justify-around items-center text-delft_blue-500 text-lg py-[0.5rem]">
             <li className="clay-button text-floral_white-900 font-robo py-2 px-3">Home</li>
