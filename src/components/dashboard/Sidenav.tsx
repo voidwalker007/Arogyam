2b6e57ec08a11e5c93054a3860eb655654b32161
voidwalker007 <akshay790570@gmail.com>
2023-11-03 22:47:06 +0530

    dashboard and sidenav added

--- src/components/dashboard/Sidenav.tsx
+++ src/components/dashboard/Sidenav.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Sidenav = () => {
   return (
-    <div className="bg-gray-200 w-1/4 h-screen p-4">
+    <div className="bg-gray-200 w-1/12 h-screen p-4 border-r">
     {/* Add your navigation tabs here */}
   </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard.tsx'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+  }, {
+    path: "/dashboard",
+    element:<Dashboard/>,
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider  router={router} />
+
+
   </React.StrictMode>,
 )
