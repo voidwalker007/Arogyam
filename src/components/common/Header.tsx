487a944442a795285e5436662c6a9c129da9e4db
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-11-03 13:38:36 +0530

    landing page draft
    - images added
    - main button card in landing page added

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,11 +2,11 @@
 export const Header = () => {
   return (
     <>
-      <header className="relative z-[999]">
+      <header className="relative z-[999] bg-white">
         <nav className=" fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2  
                     sm:top-[1.7rem] sm:h-[initial]  lg:w-[50%] w-[80%] mx-auto  rounded-full 
                   clay-card  py-[0.2rem]">
-          <ul className="flex justify-around items-center text-delft_blue-500 text-lg  py-[0.5rem]">
+          <ul className="flex justify-around items-center text-delft_blue-500 text-lg py-[0.5rem]">
             <li className="clay-button text-floral_white-900 font-robo py-2 px-3">Home</li>
             <li className="font-robo py-2 px-3">About</li>
             <li className="font-robo py-2 px-3">Contact</li>
