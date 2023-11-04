5041795ea6cb674f14ee77a8dd70862a088a84e9
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-11-05 03:39:22 +0530

    Sign Up Created, User Info to Appwrite
    - routing added for landing page, signup and login
    - eslint issue fixed
    - react router dom added
    - uuid added for creating unique ids
    - prettier dev dependency added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter ,Routes, Route} from 'react-router-dom'
 import './App.css'
+import { BrowserRouter ,Routes, Route} from 'react-router-dom'
 import LandingPage from './components/pages/LandingPage'
+import Register from './components/authentication/Register'
+import Login from './components/authentication/Login'
 
 function App() {
-
-
   return (
-
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-      <Route path='/sign-up' element = {''}/> 
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/sign-up' element = {<Register/>}/> 
+        <Route path='/login' element = {<Login/>}/> 
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
