29c8c792c70859cf7dcf2fae59eef59b3adaf9a9
voidwalker007 <akshay790570@gmail.com>
2023-11-07 02:11:23 +0530

    dashboard sidenav ui updated

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -6,19 +6,20 @@ function Dashboard() {
       <Sidenav />
       <div className="flex w-full mx-5 main-button-card h-full gap-5 ">
         <div className="w-3/4 ">
-          <div className="flex w-full flex-col h-full border border-red-500 gap-5 py-5 mx-5 ">
-            <div className="top-header  h-32 border border-blue-500 ">
-
-            <div className="flex justify-between w-full">
-        <h1 className="w-half border border-green-500">Hello user</h1>
-        <div className="w-half border border-green-500">
-          Search box
-        </div>
+          <div className="flex w-full flex-col h-full  gap-5 py-5 mx-5 ">
+            <div className="top-header  h-16 border border-blue-500 ">
+              <div className="flex justify-between items-center h-full w-full gap-10">
+                <h1 className="flex-1 text-3xl text-center">
+                    Hello User
+                </h1>
+                <div className="flex-1 rounded-md">
+                    
+                   <input className="rounded-md items-center w-full"/> 
+                
+                </div>
+              </div>
             </div>
-
-
-            </div>
-            <div className="flex-1 h-full border border-sky-500">1sdt</div>
+            <div className="flex-1 h-full border border-sky-500">1st</div>
             <div className="flex-1 h-full border border-sky-500">2nd</div>
             <div className="flex-1 h-full border border-sky-500">3rd</div>
           </div>
--- src/components/dashboard/Sidenav.tsx
+++ src/components/dashboard/Sidenav.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Sidenav = () => {
   return (
-    <div className=" main-button-card bg-gray-200 w-1/12 h-full border-x-4 rounded-full p-4">
-
-      
-    {/* Add your navigation tabs here */}
-  </div>
-  )
-}
+    <div className=" main-button-card bg-gray-200 w-1/12 h-full rounded-full p-4">
+      <ul className=" h-full mx-4 w-20 gap-10 flex  flex-col justify-center align-middle">
+        <li>
+          <a href="#" className="block p-4 hover:bg-gray-300">
+            Tab 1
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block p-4 hover:bg-gray-300">
+            Tab 2
+          </a>
+        </li>
+        <li>
+          <a href="#" className="block p-4 hover:bg-gray-300">
+            Tab 3
+          </a>
+        </li>
+        <li>
+          <Link to="/profile">
+            <a
+              href="#"
+              className="block p-4 main-clay-button content-stretch hover:bg-blue-300"
+            >
+              profile
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/logout">
+            <a
+              href="#"
+              className="block p-4 main-clay-button content-stretch hover:bg-blue-300"
+            >
+              logout
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
